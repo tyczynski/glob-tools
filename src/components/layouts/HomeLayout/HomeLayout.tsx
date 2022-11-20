aa36d637da7183f3cf76e75f4bf93a558119e38f
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2022-11-20 21:21:26 +0100

    Temporary `@ts-ignore`

--- src/components/layouts/HomeLayout/HomeLayout.tsx
+++ src/components/layouts/HomeLayout/HomeLayout.tsx
@@ -3,7 +3,9 @@ import { type PropsWithChildren } from 'react';
 import * as styles from './HomeLayout.module.scss';
 
 export const HomeLayout = ({ children }: PropsWithChildren) => (
+  // @ts-ignore
   <div className={styles.container}>
+    {/* @ts-ignore */}
     <div className={styles.innerContainer}>{children}</div>
   </div>
 );
